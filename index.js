abab1bf9431fcdb7d2185e0f2aa950c08a5ac03d
the_grode <graymafutala@gmail.com>
2023-08-14 21:58:23 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const btnMenu = document.querySelector('.mnmlst-header__menu-btn');
-const mobileMenu = document.querySelector('.mnmlst-header__menu-links--mobile');
-
-// Show mobile menu
-btnMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('mnmlst-header__menu-links--mobile--active');
-});
